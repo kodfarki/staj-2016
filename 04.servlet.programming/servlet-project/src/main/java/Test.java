c19bee2558a60cbf1d42f2113939bc7edb62c422
Mustafa Sarac <saracmustafa95@gmail.com>
2016-08-04 15:22:46 +0300

    properties modified

--- 04.servlet.programming/servlet-project/src/main/java/Test.java
+++ 04.servlet.programming/servlet-project/src/main/java/Test.java
@@ -1,14 +1,21 @@
 import com.servlet.util.JDBCUtil;
 
+import java.io.FileInputStream;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.InputStream;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.Properties;
 
 /**
  * Created by GUNEY on 4.08.2016.
  */
 public class Test {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
+
+
         try {
             PreparedStatement ps = JDBCUtil.getConnection().prepareStatement("SELECT * FROM SLCM_CAMPAIGN");
             ResultSet rs = ps.executeQuery();
@@ -17,6 +24,7 @@ public class Test {
             }
         } catch (SQLException e) {
             e.printStackTrace();
+
         }
     }
 
