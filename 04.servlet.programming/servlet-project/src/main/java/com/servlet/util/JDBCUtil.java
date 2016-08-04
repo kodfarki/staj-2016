2862cd9dfec5861117fc2c33cd01b01a1626aa20
Mustafa Sarac <saracmustafa95@gmail.com>
2016-08-04 15:36:24 +0300

    JSP created

--- 04.servlet.programming/servlet-project/src/main/java/com/servlet/util/JDBCUtil.java
+++ 04.servlet.programming/servlet-project/src/main/java/com/servlet/util/JDBCUtil.java
@@ -26,6 +26,7 @@ public class JDBCUtil {
     private JDBCUtil() {
 
 <<<<<<< HEAD
+<<<<<<< HEAD
         try {
             input = new FileInputStream("/Users/hamitsarac/projects/staj-2016/04.servlet.programming/servlet-project/config.properties");
 =======
@@ -33,6 +34,11 @@ public class JDBCUtil {
         try {
             input = new FileInputStream("config.properties");
 >>>>>>> 9ef94c9abc38ff5aec810f3998e3e708b8300d65
+=======
+
+        try {
+            input = new FileInputStream("config.properties");
+>>>>>>> d9ea02302a5da14c9f67b0066b96924f46352172
             props.load(input);
 
             driverClassName = props.getProperty("driverClassName");
@@ -49,9 +55,12 @@ public class JDBCUtil {
 
     public static Connection getConnection() throws IOException {
 <<<<<<< HEAD
+<<<<<<< HEAD
 
 =======
 >>>>>>> 9ef94c9abc38ff5aec810f3998e3e708b8300d65
+=======
+>>>>>>> d9ea02302a5da14c9f67b0066b96924f46352172
         if (connection == null) {
             try {
                 connection = DriverManager.getConnection(url, userName, password);
