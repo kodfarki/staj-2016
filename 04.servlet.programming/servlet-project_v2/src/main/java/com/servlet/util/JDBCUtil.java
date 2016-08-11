b3404ab11770328d0e26dbfdc0b1ca45242fa07b
Mustafa Sarac <saracmustafa95@gmail.com>
2016-08-11 15:02:49 +0300

    Updated

---
+++ 04.servlet.programming/servlet-project_v2/src/main/java/com/servlet/util/JDBCUtil.java
@@ -0,0 +1,74 @@
+package com.servlet.util;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.Properties;
+
+/**
+ * Created by GUNEY on 2.08.2016.
+ */
+public class JDBCUtil {
+    private static JDBCUtil me = new JDBCUtil();
+    private static Connection connection;
+
+    Properties props = new Properties();
+    InputStream input = null;
+
+    static String driverClassName;
+    static String url;
+    static String userName;
+    static String password;
+
+    private JDBCUtil() {
+
+        try {
+
+            input = new FileInputStream("/Users/hamitsarac/projects/staj-2016/04.servlet.programming/servlet-project/config.properties");
+
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+            throw new RuntimeException(e);
+        }
+
+        try {
+
+            props.load(input);
+
+            driverClassName = props.getProperty("driverClassName");
+            url = props.getProperty("url");
+            userName = props.getProperty("userName");
+            password = props.getProperty("password");
+
+            Class.forName(driverClassName);
+        } catch (Exception e) {
+            e.printStackTrace();
+            System.out.println("Driver Not Found");
+        }
+    }
+
+    public static Connection getConnection() throws IOException {
+
+        if (connection == null) {
+            try {
+                connection = DriverManager.getConnection(url, userName, password);
+            } catch (SQLException e) {
+                e.printStackTrace();
+                throw new RuntimeException(e);
+            }
+        }
+        return connection;
+    }
+
+    public static void closeConnection() {
+        try {
+            connection.close();
+        } catch (Exception exception) {
+            exception.printStackTrace();
+        }
+    }
+}
